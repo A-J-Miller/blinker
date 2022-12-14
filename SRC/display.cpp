c3d9f47b97c8fa1e2acae67d105ddea6d9f304f0
A-J-Miller <1380847@cityplym.ac.uk>
2022-12-14 10:16:40 +0000

    Update Display CPP

--- SRC/display.cpp
+++ SRC/display.cpp
@@ -7,14 +7,18 @@ extern things_t myData;
 DigitalOut green(GREEN_LED);
 
 void displayTask() {
+  cout << "\033\x63"; // Clears the console
   while (true) {
+    cout << "\033[H]"; // Returns the cursor to the start of the console
     if (myData.buttonState == true) {
-      cout << "The button is pressed" << endl;
+      cout << "The button is pressed    " << endl;
       green = true;
     } else {
       cout << "The button is not pressed" << endl;
       green = false;
     }
+    cout << "Current Temperature is: " << myData.tempC << "*c     " << endl;
+    cout << "Current Light Level is: " << myData.light << "       " << endl;
     ThisThread::sleep_for(500ms);
   }
 }
